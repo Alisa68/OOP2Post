477249f3cead4d071f90a79b9edaef4245bf0c41
Alisa68 <lebedeva19682@yandex.ru>
2021-12-26 15:12:15 +0300

    Added manager.PostManager

---
+++ src/main/java/ru/netology/maganer/PostManager.java
@@ -0,0 +1,23 @@
+package ru.netology.maganer;
+
+import ru.netology.domain.WritingOnTheWall;
+
+public class PostManager {
+    WritingOnTheWall[] writingOnTheWalls;
+
+
+    public WritingOnTheWall[] getWritingOnTheWallsToArray() {
+        // to do;
+        return null;
+    }
+
+
+    public WritingOnTheWall[] search(int ownerId, int id) {
+        // to do;
+        return null;
+    }
+
+    public void delete(int ownerId, int id) {
+        //to find the post in Array and delete it
+    }
+}
